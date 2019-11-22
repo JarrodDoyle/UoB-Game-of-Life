4298d44ccbccf0c3734ff6cef1dcef2b11c0459e
Jarrod Doyle <ff18713@bristol.ac.uk>
2019-11-22 20:12:05 +0000

    Tests for 6/8/12 threads added

--- main_test.go
+++ main_test.go
@@ -47,10 +47,58 @@ func Test(t *testing.T) {
 			},
 		}},
 
+		{"16x16x6-0", args{
+			p: golParams{
+				turns:       0,
+				threads:     6,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 4, y: 5},
+				{x: 5, y: 6},
+				{x: 3, y: 7},
+				{x: 4, y: 7},
+				{x: 5, y: 7},
+			},
+		}},
+
 		{"16x16x8-0", args{
 			p: golParams{
 				turns:       0,
 				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 4, y: 5},
+				{x: 5, y: 6},
+				{x: 3, y: 7},
+				{x: 4, y: 7},
+				{x: 5, y: 7},
+			},
+		}},
+
+		{"16x16x10-0", args{
+			p: golParams{
+				turns:       0,
+				threads:     10,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 4, y: 5},
+				{x: 5, y: 6},
+				{x: 3, y: 7},
+				{x: 4, y: 7},
+				{x: 5, y: 7},
+			},
+		}},
+
+		{"16x16x12-0", args{
+			p: golParams{
+				turns:       0,
+				threads:     12,
 				imageWidth:  16,
 				imageHeight: 16,
 			},
@@ -95,10 +143,58 @@ func Test(t *testing.T) {
 			},
 		}},
 
+		{"16x16x6-1", args{
+			p: golParams{
+				turns:       1,
+				threads:     6,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 3, y: 6},
+				{x: 5, y: 6},
+				{x: 4, y: 7},
+				{x: 5, y: 7},
+				{x: 4, y: 8},
+			},
+		}},
+
 		{"16x16x8-1", args{
 			p: golParams{
 				turns:       1,
 				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 3, y: 6},
+				{x: 5, y: 6},
+				{x: 4, y: 7},
+				{x: 5, y: 7},
+				{x: 4, y: 8},
+			},
+		}},
+
+		{"16x16x10-1", args{
+			p: golParams{
+				turns:       1,
+				threads:     10,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 3, y: 6},
+				{x: 5, y: 6},
+				{x: 4, y: 7},
+				{x: 5, y: 7},
+				{x: 4, y: 8},
+			},
+		}},
+
+		{"16x16x12-1", args{
+			p: golParams{
+				turns:       1,
+				threads:     12,
 				imageWidth:  16,
 				imageHeight: 16,
 			},
@@ -143,10 +239,58 @@ func Test(t *testing.T) {
 			},
 		}},
 
+		{"16x16x6-100", args{
+			p: golParams{
+				turns:       100,
+				threads:     6,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 12, y: 0},
+				{x: 13, y: 0},
+				{x: 14, y: 0},
+				{x: 13, y: 14},
+				{x: 14, y: 15},
+			},
+		}},
+
 		{"16x16x8-100", args{
 			p: golParams{
 				turns:       100,
 				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 12, y: 0},
+				{x: 13, y: 0},
+				{x: 14, y: 0},
+				{x: 13, y: 14},
+				{x: 14, y: 15},
+			},
+		}},
+
+		{"16x16x10-100", args{
+			p: golParams{
+				turns:       100,
+				threads:     10,
+				imageWidth:  16,
+				imageHeight: 16,
+			},
+			expectedAlive: []cell{
+				{x: 12, y: 0},
+				{x: 13, y: 0},
+				{x: 14, y: 0},
+				{x: 13, y: 14},
+				{x: 14, y: 15},
+			},
+		}},
+
+		{"16x16x12-100", args{
+			p: golParams{
+				turns:       100,
+				threads:     12,
 				imageWidth:  16,
 				imageHeight: 16,
 			},
@@ -189,123 +333,123 @@ func Benchmark(b *testing.B) {
 	}{
 		{
 			"16x16x2", golParams{
-			turns:       benchLength,
-			threads:     2,
-			imageWidth:  16,
-			imageHeight: 16,
-		}},
+				turns:       benchLength,
+				threads:     2,
+				imageWidth:  16,
+				imageHeight: 16,
+			}},
 
 		{
 			"16x16x4", golParams{
-			turns:       benchLength,
-			threads:     4,
-			imageWidth:  16,
-			imageHeight: 16,
-		}},
+				turns:       benchLength,
+				threads:     4,
+				imageWidth:  16,
+				imageHeight: 16,
+			}},
 
 		{
 			"16x16x8", golParams{
-			turns:       benchLength,
-			threads:     8,
-			imageWidth:  16,
-			imageHeight: 16,
-		}},
+				turns:       benchLength,
+				threads:     8,
+				imageWidth:  16,
+				imageHeight: 16,
+			}},
 
 		{
 			"64x64x2", golParams{
-			turns:       benchLength,
-			threads:     2,
-			imageWidth:  64,
-			imageHeight: 64,
-		}},
+				turns:       benchLength,
+				threads:     2,
+				imageWidth:  64,
+				imageHeight: 64,
+			}},
 
 		{
 			"64x64x4", golParams{
-			turns:       benchLength,
-			threads:     4,
-			imageWidth:  64,
-			imageHeight: 64,
-		}},
+				turns:       benchLength,
+				threads:     4,
+				imageWidth:  64,
+				imageHeight: 64,
+			}},
 
 		{
 			"64x64x8", golParams{
-			turns:       benchLength,
-			threads:     8,
-			imageWidth:  64,
-			imageHeight: 64,
-		}},
+				turns:       benchLength,
+				threads:     8,
+				imageWidth:  64,
+				imageHeight: 64,
+			}},
 
 		{
 			"128x128x2", golParams{
-			turns:       benchLength,
-			threads:     2,
-			imageWidth:  128,
-			imageHeight: 128,
-		}},
+				turns:       benchLength,
+				threads:     2,
+				imageWidth:  128,
+				imageHeight: 128,
+			}},
 
 		{
 			"128x128x4", golParams{
-			turns:       benchLength,
-			threads:     4,
-			imageWidth:  128,
-			imageHeight: 128,
-		}},
+				turns:       benchLength,
+				threads:     4,
+				imageWidth:  128,
+				imageHeight: 128,
+			}},
 
 		{
 			"128x128x8", golParams{
-			turns:       benchLength,
-			threads:     8,
-			imageWidth:  128,
-			imageHeight: 128,
-		}},
+				turns:       benchLength,
+				threads:     8,
+				imageWidth:  128,
+				imageHeight: 128,
+			}},
 
 		{
 			"256x256x2", golParams{
-			turns:       benchLength,
-			threads:     2,
-			imageWidth:  256,
-			imageHeight: 256,
-		}},
+				turns:       benchLength,
+				threads:     2,
+				imageWidth:  256,
+				imageHeight: 256,
+			}},
 
 		{
 			"256x256x4", golParams{
-			turns:       benchLength,
-			threads:     4,
-			imageWidth:  256,
-			imageHeight: 256,
-		}},
+				turns:       benchLength,
+				threads:     4,
+				imageWidth:  256,
+				imageHeight: 256,
+			}},
 
 		{
 			"256x256x8", golParams{
-			turns:       benchLength,
-			threads:     8,
-			imageWidth:  256,
-			imageHeight: 256,
-		}},
+				turns:       benchLength,
+				threads:     8,
+				imageWidth:  256,
+				imageHeight: 256,
+			}},
 
 		{
 			"512x512x2", golParams{
-			turns:       benchLength,
-			threads:     2,
-			imageWidth:  512,
-			imageHeight: 512,
-		}},
+				turns:       benchLength,
+				threads:     2,
+				imageWidth:  512,
+				imageHeight: 512,
+			}},
 
 		{
 			"512x512x4", golParams{
-			turns:       benchLength,
-			threads:     4,
-			imageWidth:  512,
-			imageHeight: 512,
-		}},
+				turns:       benchLength,
+				threads:     4,
+				imageWidth:  512,
+				imageHeight: 512,
+			}},
 
 		{
 			"512x512x8", golParams{
-			turns:       benchLength,
-			threads:     8,
-			imageWidth:  512,
-			imageHeight: 512,
-		}},
+				turns:       benchLength,
+				threads:     8,
+				imageWidth:  512,
+				imageHeight: 512,
+			}},
 	}
 	for _, bm := range benchmarks {
 		os.Stdout = nil // Disable all program output apart from benchmark results
